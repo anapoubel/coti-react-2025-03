929e18b43dba728e6a2fe2da1c447de7fcd4bc7d
Ana Poubel <anaclaragp@gmail.com>
2025-04-22 21:23:50 -0300

    deploy

--- raw/src/components/Footer/index.jsx
+++ raw/src/components/Footer/index.jsx
@@ -1,8 +1,13 @@
 import * as S from './style'
+import { NavLink, useHref } from 'react-router-dom'
 
 // JS-to-CSS
 const Footer = () => {
-  return <S.FooterContainer>Projeto by Coti Informática</S.FooterContainer>
+  
+  return <S.FooterContainer>
+    {url !== '/' && <NavLink to="/">Voltar</NavLink>}
+    Projeto by Coti Informática
+    </S.FooterContainer>
 }
 
 export default Footer
